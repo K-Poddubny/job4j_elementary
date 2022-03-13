1484287aededa14d0144506ef17c67bab7ae7bd1
Kpodd <k.poddubny91@gmail.com>
2022-03-13 23:50:28 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Konstantin Poddubny");
+        System.out.println("03.03.1991");
+    }
+}
