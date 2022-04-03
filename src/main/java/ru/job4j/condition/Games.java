dc73abd09fa0d8b3351854a1534feeb294044394
Kpodd <k.poddubny91@gmail.com>
2022-04-04 00:25:36 +0300

    Оператор && [#333783 #279578]

--- src/main/java/ru/job4j/condition/Games.java
+++ src/main/java/ru/job4j/condition/Games.java
@@ -7,11 +7,6 @@ public class Games {
         } else {
             System.out.println("I can't.");
         }
-        if (allowByParent && hasMoney) {
-            System.out.println("I can go to the computer club.");
-        } else {
-            System.out.println("I can't.");
-        }
     }
 
     public static void main(String[] args) {
