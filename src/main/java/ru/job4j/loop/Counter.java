ec20b98ce68d510cfc1074cb0c5583f93ce2b3ba
Kpodd <k.poddubny91@gmail.com>
2022-04-10 21:24:41 +0300

    Сумма чисел. [#226416]

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+import java.security.PublicKey;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int result = start; result <= finish; result++) {
+            sum = sum + result;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+    }
+}
