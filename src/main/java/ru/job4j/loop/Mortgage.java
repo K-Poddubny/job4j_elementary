62cc12c5a496400f42370b6d7f45b6714c88cde5
Kpodd <k.poddubny91@gmail.com>
2022-04-17 18:22:50 +0300

    Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -6,7 +6,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = (amount + percent) - salary;
+            amount = amount + (amount * percent / 100) - salary;
                year++;
             }
         return year;
