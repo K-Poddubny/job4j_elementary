8c0c0edac78c889761970dbd60ea6f7445543b7c
Kpodd <k.poddubny91@gmail.com>
2022-04-21 19:00:05 +0300

    Перевернуть массив. [#226790]

---
+++ src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        for (int index = 0; index < array[(array.length - 1) - index]; index++) {
+            int temp = array[index];
+            array[index] = array[array.length - 1 - index];
+            array[array.length - 1 - index] = temp;
+            if (array[index] == array[(array.length - 1) / 2]) {
+                break;
+            }
+        }
+        return array;
+    }
+}
