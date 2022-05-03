4fd1e01614b54b6646fe73545c77f6080252e2e3
Kpodd <k.poddubny91@gmail.com>
2022-05-03 13:16:21 +0300

    Слово начинается с ... [#41585 #293094]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,13 +5,8 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] perf) {
         boolean result = true;
         for (int index = 0; index < perf.length; index++) {
-            for (int index2 = 0; index2 < word.length; index2++) {
-                if (perf[index] == word[index2]) {
-                    result = true;
-                    break;
-                } else {
-                    result = false;
-                }
+            if (perf[index] != word[index]) {
+                result = false;
             }
         }
         return result;
